f95738b9afc390b844992a47c7703cd4328701d3
Dexshine <b_o_y_s@hotmail.com>
2020-02-07 15:36:20 +0700

    fix min max

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -20,16 +20,18 @@ int main()
 //Write definition of stat() here 
 void stat(const double A[],int sizeArrayA,double B[])
 {   
-    double sum=0,std=0,max,min,zsquarex;
+    double sum=0,std1=0,max=A[0],min=A[0],zsquarex=0;
     for(int i=0;i<sizeArrayA;i++){
         sum += A[i];
-        if(A[i]>A[i-1]) max = A[i];
-        if(A[i]<A[i-1]) min = A[i];
+        if(A[i]>max) max = A[i];
+        if(A[i]<min) min = A[i];
         zsquarex += pow(A[i],2);
     }
-    std = sqrt(((1/sizeArrayA) * (zsquarex)) - pow(sum/sizeArrayA,2));
+    double mean;
+    mean = sum/sizeArrayA;
+    std1 = sqrt(((1/sizeArrayA) * (zsquarex)) - pow(mean,2));
     B[0] = sum/sizeArrayA;
-    B[1] = std;
+    B[1] = std1;
     B[2] = max;
     B[3] = min;
 

d42e3a1ff67a8dd02147db34196d0d0f152a5efc
Dexshine <b_o_y_s@hotmail.com>
2020-02-07 15:25:04 +0700

    ver1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -18,3 +18,19 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double A[],int sizeArrayA,double B[])
+{   
+    double sum=0,std=0,max,min,zsquarex;
+    for(int i=0;i<sizeArrayA;i++){
+        sum += A[i];
+        if(A[i]>A[i-1]) max = A[i];
+        if(A[i]<A[i-1]) min = A[i];
+        zsquarex += pow(A[i],2);
+    }
+    std = sqrt(((1/sizeArrayA) * (zsquarex)) - pow(sum/sizeArrayA,2));
+    B[0] = sum/sizeArrayA;
+    B[1] = std;
+    B[2] = max;
+    B[3] = min;
+
+}
